a99df73bb4b3f4d3089f4c8648a59401b6fa4691
sunnyabc789 <420095734@qq.com>
2021-12-09 23:56:43 +0800

    save

--- webpack/plugin/removeAlicdn.js
+++ webpack/plugin/removeAlicdn.js
@@ -10,6 +10,27 @@ class RemoveAlicdnPlugin {
         compiler.hooks.shouldEmit.tap('RemoveAlicdn', (compilation) => {
             console.log(compilation.assets['css/app.css'].children[0]._value.includes('alicdn'),'1===')
         })   
+
+        // 这个写法感觉更好
+         compiler.hooks.emit.tap("RemoveDataTest", (compilation) => {
+           Object.keys(compilation.assets).forEach((filename) => {
+             // step4: 得到资源内容
+             let content = compilation.assets[filename].source();
+             // step5: 清除 html 文件中的 data-test 属性
+             if (/\.html$/.test(filename)) {
+               content = content.replace(reg, "");
+             }
+             // step6: 更新 compilation.assets[filename] 对象
+             compilation.assets[filename] = {
+               source() {
+                 return content;
+               },
+               size() {
+                 return content.length;
+               },
+             };
+           });
+         });
     }
 }
 
