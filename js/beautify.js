4b5f50ad0f07b1b2379bb6d4260ac47111a75b53
chenyongqing.cyq <chenyongqing.cyq@alibaba-inc.com>
2023-06-15 16:30:22 +0800

    save

---
+++ js/beautify.js
@@ -0,0 +1,17 @@
+// 代码格式化
+{/* <script src="https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.14.7/beautify.min.js"></script> */}
+const options = {
+    "indent_size": "1",
+    "indent_char": "\t",
+    "wrap_line_length": "0",
+    "brace_style": "collapse",
+    "space_before_conditional": true,
+    "space_after_named_function": false,
+    "space_after_anon_function": false,
+    "space_in_paren": false,
+    "break_chained_methods": true,
+    "end_with_newline": false,
+    "e4x": true
+}
+
+js_beautify(code, options)
