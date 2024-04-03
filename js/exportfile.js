ddc8bb0cd2a1e9f686b0e8f453afd14ed66db6b6
chenyongqing.cyq <chenyongqing.cyq@cainiao.com>
2024-04-03 17:36:40 +0800

    save

--- js/exportfile.js
+++ js/exportfile.js
@@ -1,3 +1,7 @@
+// createObjectURL
+// 它可以将Blob、File等二进制文件转换为浏览器可以直接显示的URL地址，从而方便进行展示、下载等操作 
+// 图片可以放到img标签  pdf等window.open()
+
 function exportFile(filename, textContent, type = 'text/csv;charset=utf-8;') {
     if (!_.startsWith(textContent, '\uFEFF')) {
       textContent = '\uFEFF' + textContent
@@ -42,11 +46,12 @@ function utf8ToBinary(utf8String) {
 
 function dataURLtoBlob(dataurl) {
     // 如果没给编码信息 可以不需要这段
-	var arr = dataurl.split(","),
-		mime = arr[0].match(/:(.*?);/)[1],
-		bstr = atob(arr[1]),
-		n = bstr.length,
-		u8arr = new Uint8Array(n);
+	let arr = dataurl.split(",");
+  // application/pdf
+	let mime = arr[0].match(/:(.*?);/)[1] || 'image/png';
+	let bstr = atob(arr[1]);
+	let n = bstr.length;
+	let u8arr = new Uint8Array(n);
 	while (n--) {
 		u8arr[n] = bstr.charCodeAt(n);
 	}
